ef61974f7a62050f5b212f223ca281bfe09dce56
John O'Connor <oconnojb@gmail.com>
2019-01-13 23:47:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ class Meal {
   }
 
   static byPrice() {
-    store.meals.sort(function compareNumbers(a, b) {
+    return store.meals.sort(function compareNumbers(a, b) {
   return a.price - b.price;
 })
   }
