e36184c8bb9e8ab57ad28356335e9a76fc301f88
John O'Connor <oconnojb@gmail.com>
2019-01-13 23:01:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ class Customer {
         newStore.meals.push(store.meals.find(
           function(meal) {
             if (meal.id === element) {
-              console.log(meal)
+              console.log(meal.title)
             }
           }
         ))
